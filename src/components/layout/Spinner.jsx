e9e1a11dbb2b1b27d32318986add44b1a591d51b
steven kim <steven.sy.kim@gmail.com>
2022-03-22 23:53:50 -0400

    changing auth token

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -2,7 +2,7 @@ import spinner from './assets/spinner.gif'
 
 function Spinner() {
   return (
-    <div classname='w-10 mt-20'>
+    <div className='w-10 mt-20'>
       <img 
         width={180} 
         className='text-center mx-auto' 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,10 +1,11 @@
 import {useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
-import GithubContext, { GithubProvider } from '../../context/github/GithubContext'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
   const { users, loading, fetchUsers } = useContext(GithubContext)
+
   useEffect(() => {
     fetchUsers()
   }, [])
